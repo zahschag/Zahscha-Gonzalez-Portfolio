472876d410025c03bf838d65dd09a2e0976aa691
Zahscha <zahscha.gonzalez@gmail.com>
2013-09-16 18:13:03 -0400

    attribute selection
    Reduced the lines of code created in order to reduce the code for efficiency.

--- js/site.js
+++ js/site.js
@@ -1,15 +1,7 @@
 $(function(){
 
 
-	var links = $('nav li');
-	
-	var home = links.eq(0).html("<a href='main.html'>Home</a>");
-	var services = links.eq(1).html("<a href='#services'>Services</a>");
-	var projects = links.eq(2).html("<a href='#projects'>Projects</a>");
-	var aboutMe = links.eq(3).html("<a href='#about_me'>About Me</a>");
-	var contactMe = links.eq(4).html("<a href='contact_me'><Contact Me</a>");
-	var div = $('div');
-	var divpc = div.filter('.placeholder');
-	console.log(divpc);
+	var links = $('a[href="#"]');
+	console.log(links);
 
 });

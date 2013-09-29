73206046d575e36fffe4a55d683f14a8f211d735
Zahscha <zahscha.gonzalez@gmail.com>
2013-09-29 17:38:03 -0400

    speed change

--- js/site.js
+++ js/site.js
@@ -19,7 +19,7 @@ $(function(){
       if (target.length) {
         $('html,body').animate({
           scrollTop: target.offset().top
-        }, 1000);
+        }, 1500);
         return false;
       }
     }

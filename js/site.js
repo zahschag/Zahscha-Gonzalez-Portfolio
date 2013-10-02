c231d72e9517f01375bf8dc6a96b0afdf8dba1a4
Zahscha Gonzalez <zahscha.gonzalez@gmail.com>
2013-10-02 12:04:24 -0400

    Responsive for large screens
    -Added media queries for large screen

--- js/site.js
+++ js/site.js
@@ -1,14 +1,30 @@
 $(function(){
 
 	$.scrollUp();
+  $('form').bind('submit', function(){
+    return false;
+  });
 	var links = $('nav li');
-	var backTopbtn = $('button');
-	
 	var home = links.eq(0).wrap("<a href='main.html'></a>");
 	var services = links.eq(1).wrap("<a href='#services'></a>");
 	var projects = links.eq(2).wrap("<a href='#projects'></a>");
-	var aboutMe = links.eq(3).wrap("<a href='#aboutme'></a>");
+	var aboutMe = links.eq(3).wrap("<a href='#about_me'></a>");
 	var contactMe = links.eq(4).wrap("<a href='#contact_me'></a>");
+
+/*Fancy box plug in begins*/
+  $('.placeholder').fancybox({
+      openEffect : 'fade',
+      closeEffect : 'fade',
+      closeBtn : true,
+      arrows : true,
+  });
+/*Smooth scrolling begins*/
+//Targets the attr href that is selected
+//added a click function
+//captures the location of the target
+/* uses a conditional to test the offset top position of the id attr and scrolls directly
+using a an animation*/
+//returns false to escape the operation.
 	$('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
         || location.hostname == this.hostname) {
@@ -17,7 +33,7 @@ $(function(){
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
         $('html,body').animate({
-          scrollTop: target.offset().top
+          scrollTop: target.offset().top 
         },{
           duration:500,
           specialEasing:{
@@ -29,14 +45,5 @@ $(function(){
       }
     }
   });
-  
-  $('.placeholder').fancybox({
-      openEffect : 'fade',
-      closeEffect : 'fade',
-      closeBtn : true,
-      arrows : true,
-      nextClick: true,
-      
-  });
-
+//Form validation
 });
